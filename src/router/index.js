e4c27e7dbf1ac19eb9a5e913b1e16ffa6c346819
Arif Samed İPEK <arifsamedipek@gmail.com>
2020-04-21 22:20:03 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import routes from './routes';
 Vue.use(Router);
 
 const router = new Router({
-  scrollBehavior: () => ({ y: 0}),
+  scrollBehavior: () => ({ y: 0 }),
   mode: 'history',
   routes,
 });
